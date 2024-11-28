5987caa7d3b9e0f734889cb481cc5a0df503d350
Thomas Fabbris <115624667+Thomas-Fabbris@users.noreply.github.com>
2024-11-28 17:54:26 +0100

    Create class SportManagerDB

--- Codice/SportMateDBLayer/src/main/java/SportMateInc/SportMateDBLayer/SportMateDB.java
+++ Codice/SportMateDBLayer/src/main/java/SportMateInc/SportMateDBLayer/SportMateDB.java
@@ -1,17 +1,39 @@
 package SportMateInc.SportMateDBLayer;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class SportMateDB {
-	public static String DB_REL_FILE = "resources/SportMate.db";
-	public static String DB_URL = "jdbc:sqlite:" + DB_REL_FILE;
-	//Usa singleton
+	private Connection connection;
+	private static SportMateDB database = null;
+	private static final String DB_REL_FILE = "../resources/SportMate.db";
+	private static final String DB_URL = "jdbc:sqlite:" + DB_REL_FILE;
+	private static final Logger LOGGER = LogManager.getLogger(SportMateDB.class);
 	
-	public SportMate() {
-		apriConnessione();
+	
+	
+	public static SportMateDB instance() {
+		if(database == null) {
+				database = new SportMateDB();
+		}
+		return database;
 	}
 	
-	public Boolean apriConnessione() {
-		return true;
+	private SportMateDB(){
+		try {
+		connection = DriverManager.getConnection(DB_URL);
+		if(connection != null) {
+			LOGGER.info("Connection to SportMateDB successfully started!");
+		}
+	} catch (SQLException e) {
+		LOGGER.info(String.format("Connection to SportMateDB failed: %s",e.getMessage()));
 	}
-	
+	}
 	
 }
