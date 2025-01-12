2f99a1148ac692118cc6715eff4a0d6d73aa6469
Thomas Fabbris <115624667+Thomas-Fabbris@users.noreply.github.com>
2025-01-13 00:48:24 +0100

    Iniziato login

--- Codice/SportMateBusinessLayer/src/main/java/SportMateInc/SportMateBusinessLayer/GestoriService.java
+++ Codice/SportMateBusinessLayer/src/main/java/SportMateInc/SportMateBusinessLayer/GestoriService.java
@@ -8,18 +8,17 @@ import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 import org.jooq.Record;
 
-import SportMateInc.SportMateBusinessLayer.tables.Utenti;
-public class UtentiService {
+import SportMateInc.SportMateBusinessLayer.tables.Gestori;
+public class GestoriService {
 	
 	public static Record findByUsername(String username) {
 		SportMateDB db = SportMateDB.getInstance();
 		db.apriConnessione();
 		DSLContext create = DSL.using(db.getConnectionDetails(), SQLDialect.SQLITE);
 		Record result = create.select()
-		.from(Utenti.UTENTI)
-		.where(Utenti.UTENTI.MAIL.eq(username)).fetchOne();
+		.from(Gestori.GESTORI)
+		.where(Gestori.GESTORI.MAIL.eq(username)).fetchOne();
 		db.chiudiConnessione();
 		return result;
 	}
-	
 }
--- Codice/SportMatePresentationLayer/src/main/java/sportmateinc/sportmatepresentationlayer/application/security/UserDetailsServiceImpl.java
+++ Codice/SportMatePresentationLayer/src/main/java/sportmateinc/sportmatepresentationlayer/application/security/UserDetailsServiceImpl.java
@@ -1,7 +1,7 @@
 package sportmateinc.sportmatepresentationlayer.application.security;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,34 +10,37 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import sportmateinc.sportmatepresentationlayer.application.data.User;
-import sportmateinc.sportmatepresentationlayer.application.data.UserRepository;
+import org.jooq.Record;
+
+import SportMateInc.SportMateBusinessLayer.GestoriService;
+import SportMateInc.SportMateBusinessLayer.UtentiService;
+import SportMateInc.SportMateBusinessLayer.tables.Gestori;
+import SportMateInc.SportMateBusinessLayer.tables.Utenti;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final UserRepository userRepository;
-
-    public UserDetailsServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+    	Record user = UtentiService.findByUsername(username);
         if (user == null) {
-            throw new UsernameNotFoundException("No user present with username: " + username);
+        	user = GestoriService.findByUsername(username);
+        	if(user == null) {
+        		 throw new UsernameNotFoundException("No user present with username: " + username);
+        	}
+        	else {
+        		return new org.springframework.security.core.userdetails.User(user.field(Gestori.GESTORI.MAIL).toString(),user.field(Gestori.GESTORI.PASSWORD).toString(),
+                        getAuthorities("GESTORE"));
+        	}
+           
         } else {
-            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getHashedPassword(),
-                    getAuthorities(user));
-        }
+        	return new org.springframework.security.core.userdetails.User(user.field(Utenti.UTENTI.MAIL).toString(),user.field(Utenti.UTENTI.PASSWORD).toString(),
+                    getAuthorities("UTENTE"));        }
     }
 
-    private static List<GrantedAuthority> getAuthorities(User user) {
-        return user.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-                .collect(Collectors.toList());
-
+    private static List<GrantedAuthority> getAuthorities(String role) {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_" + role));
     }
 
 }
