1ddb3c60b4e9218c8bf2cfb4fcf164bdec9c5b3b
Thomas Fabbris <115624667+Thomas-Fabbris@users.noreply.github.com>
2025-01-13 00:18:50 +0100

    Iniziata implementazione login

--- Codice/SportMateBusinessLayer/src/main/java/SportMateInc/SportMateBusinessLayer/GestoriService.java
+++ Codice/SportMateBusinessLayer/src/main/java/SportMateInc/SportMateBusinessLayer/GestoriService.java
@@ -1,25 +1,22 @@
 package SportMateInc.SportMateBusinessLayer;
 
-import sportmateinc.sportmatedblayer.SportMateDB;
-
-
 import org.jooq.DSLContext;
+import org.jooq.Record;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
-import org.jooq.Record;
 
-import sportmateinc.sportmatebusinesslayer.generated.tables.Utenti;
-public class UtentiService {
-	
+import sportmateinc.sportmatebusinesslayer.generated.tables.Gestori;
+import sportmateinc.sportmatedblayer.SportMateDB;
+
+public class GestoriService {
 	public static Record findByUsername(String username) {
 		SportMateDB db = SportMateDB.getInstance();
 		db.apriConnessione();
 		DSLContext create = DSL.using(db.getConnectionDetails(), SQLDialect.SQLITE);
 		Record result = create.select()
-		.from(Utenti.UTENTI)
-		.where(Utenti.UTENTI.MAIL.eq(username)).fetchOne();
+		.from(Gestori.GESTORI)
+		.where(Gestori.GESTORI.MAIL.eq(username)).fetchOne();
 		db.chiudiConnessione();
 		return result;
 	}
-	
 }
