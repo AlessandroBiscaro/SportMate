13fbd88ee6a92089de91cdeaee9d65ce3c24faf3
Thomas Fabbris <115624667+Thomas-Fabbris@users.noreply.github.com>
2025-01-14 00:09:28 +0100

    Continuato login

--- Codice/SportMateBusinessLayer/src/main/java/SportMateInc/SportMateBusinessLayer/UtentiService.java
+++ Codice/SportMateBusinessLayer/src/main/java/SportMateInc/SportMateBusinessLayer/UtentiService.java
@@ -2,22 +2,29 @@ package SportMateInc.SportMateBusinessLayer;
 
 import sportmateinc.sportmatedblayer.SportMateDB;
 
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 import org.jooq.Record;
 
-import SportMateInc.SportMateBusinessLayer.tables.Utenti;
+import static SportMateInc.SportMateBusinessLayer.tables.Utenti.UTENTI;
 public class UtentiService {
 	
+	private static final Logger LOGGER = LogManager.getLogger(UtentiService.class);
+	
+	private UtentiService() {}
+ 	
 	public static Record findByUsername(String username) {
 		SportMateDB db = SportMateDB.getInstance();
 		db.apriConnessione();
 		DSLContext create = DSL.using(db.getConnectionDetails(), SQLDialect.SQLITE);
-		Record result = create.select()
-		.from(Utenti.UTENTI)
-		.where(Utenti.UTENTI.MAIL.eq(username)).fetchOne();
+		LOGGER.debug(db.getConnectionDetails().toString());
+		Record result = create.select(UTENTI.MAIL, UTENTI.PASSWORD)
+				.from(UTENTI)
+		//.where(UTENTI.MAIL.eq(username))
+		.fetchOne();
 		db.chiudiConnessione();
 		return result;
 	}
--- Codice/SportMatePresentationLayer/src/main/java/sportmateinc/sportmatepresentationlayer/application/views/comuni/LoginView.java
+++ Codice/SportMatePresentationLayer/src/main/java/sportmateinc/sportmatepresentationlayer/application/views/comuni/LoginView.java
@@ -17,19 +17,14 @@ import sportmateinc.sportmatepresentationlayer.application.security.Authenticate
 @Route(value = "login")
 public class LoginView extends LoginOverlay implements BeforeEnterObserver {
 
-    private final AuthenticatedUser authenticatedUser;
+	private static final long serialVersionUID = 1L;
+	private final AuthenticatedUser authenticatedUser;
 
     public LoginView(AuthenticatedUser authenticatedUser) {
         this.authenticatedUser = authenticatedUser;
+        
         setAction(RouteUtil.getRoutePath(VaadinService.getCurrent().getContext(), getClass()));
-
-        LoginI18n i18n = LoginI18n.createDefault();
-        i18n.setHeader(new LoginI18n.Header());
-        i18n.getHeader().setTitle("SportMate");
-        i18n.getHeader().setDescription("Login using user/user or admin/admin");
-        i18n.setAdditionalInformation(null);
-        setI18n(i18n);
-
+        setI18n(getDescrizione());
         setForgotPasswordButtonVisible(false);
         setOpened(true);
     }
@@ -43,5 +38,14 @@ public class LoginView extends LoginOverlay implements BeforeEnterObserver {
         }
 
         setError(event.getLocation().getQueryParameters().getParameters().containsKey("error"));
+    }
+    
+    private LoginI18n getDescrizione() {
+    	LoginI18n i18n = LoginI18n.createDefault();
+        i18n.setHeader(new LoginI18n.Header());
+        i18n.getHeader().setTitle("SportMate");
+        i18n.getHeader().setDescription("Inserisci le tue credenziali di accesso a SportMate");
+        i18n.setAdditionalInformation(null);
+        return i18n;
     }
 }
